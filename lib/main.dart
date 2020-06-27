1306bddbf1c3eacd6b345bf4d26c7e5304fc46e8
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-06-27 22:30:23 +0530

    Added pages with images

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-MaterialApp(
-  home: HomePage(),
-)
-);
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,26 +11,51 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  PageController _pageController;
+
+  void _onScroll() {
+    print("scrolled");
+  }
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 0,
+    )..addListener(() {
+        _onScroll();
+      });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        children: [
-          Container(
-            color: Colors.blue
-          ),
-          Container(
-            color: Colors.red
-          ),
-          Container(
-            color: Colors.white
-          ),
-          Container(
-            color: Colors.yellow
-          ),
+        body: PageView(
+      children: [
+        makePage("assets/images/one.jpg"),
+        makePage("assets/images/two.jpg"),
+        makePage("assets/images/three.jpg"),
+        makePage("assets/images/four.jpg"),
+      ],
+    ));
+  }
 
-        ],
-      )
+  Widget makePage(image) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+          0.3,
+          0.9
+        ], colors: [
+          Colors.black.withOpacity(.9),
+          Colors.black.withOpacity(.2)
+        ])),
+      ),
     );
   }
 }
